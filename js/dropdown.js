2ad0062ccc4a838376b3351e184fe291a1232789
Allan Lannoy <allan1411lannoy@gmail.com>
2023-12-12 13:06:11 +0100

    Ajout en JS des options pour chaque dropdown + css

--- js/dropdown.js
+++ js/dropdown.js
@@ -7,15 +7,80 @@ dropdowns.forEach(dropdown => {
 
 dropdownBtn.forEach(dropdown => {
     const dropdownContent = dropdown.parentElement.querySelector(".content");
+    const angleDown = dropdown.querySelector(".fa-solid.fa-angle-down");
+    const dropdownOption = dropdownContent.querySelector(".dropdown-options");
+    const optionLi = dropdownOption.querySelectorAll(".option");
 
     dropdown.addEventListener('click', () => {
         if (dropdown.parentElement.classList.contains("active")) {
             dropdownContent.style.display = "none";
-            dropdown.parentElement.classList.remove("active")
+            dropdownOption.style.display = "none";
+            dropdown.parentElement.classList.remove("active");
+            angleDown.classList.remove("fa-angle-up");
+            angleDown.classList.add("fa-angle-down");
+
         }
         else {
             dropdown.parentElement.classList.add("active")
             dropdownContent.style.display = "block";
+            dropdownOption.style.display = "block";
+            angleDown.classList.remove("fa-angle-down");
+            angleDown.classList.add("fa-angle-up");
         }
     })
 })
+
+const allIngredients = recipes.reduce((acc, recipe) => {
+    recipe.ingredients.forEach(ingredient => {
+        if (!acc.includes(ingredient.ingredient)) {
+            acc.push(ingredient.ingredient);
+        }
+    });
+    return acc;
+}, []);
+
+const allAppliances = recipes.reduce((acc, recipe) => {
+    if (!acc.includes(recipe.appliance)) {
+        acc.push(recipe.appliance);
+    }
+    return acc;
+}, []);
+
+const allUstensils = recipes.reduce((acc, recipe) => {
+    recipe.ustensils.forEach(ustensil => {
+        if (!acc.includes(ustensil)) {
+            acc.push(ustensil);
+        }
+    });
+    return acc;
+}, []);
+
+const ingredientDropdown = document.getElementById('IngrédientsDropDown');
+allIngredients.forEach(ingredient => {
+    const dropdownOption = ingredientDropdown.querySelector(".dropdown-options")
+    const optionLi = document.createElement("li");
+    optionLi.className = 'option';
+    optionLi.textContent = ingredient;
+    dropdownOption.appendChild(optionLi);
+    ingredientDropdown.appendChild(dropdownOption);
+});
+
+const applianceDropdown = document.getElementById('AppareilsDropDown');
+allAppliances.forEach(appliance => {
+    const dropdownOption = applianceDropdown.querySelector(".dropdown-options")
+    const optionLi = document.createElement("li");
+    optionLi.className = 'option';
+    optionLi.textContent = appliance;
+    dropdownOption.appendChild(optionLi);
+    applianceDropdown.appendChild(dropdownOption);
+});
+
+const ustensilDropdown = document.getElementById('UstensilesDropDown');
+allUstensils.forEach(ustensil => {
+    const dropdownOption = ustensilDropdown.querySelector(".dropdown-options")
+    const optionLi = document.createElement("li");
+    optionLi.className = 'option';
+    optionLi.textContent = ustensil;
+    dropdownOption.appendChild(optionLi);
+    ustensilDropdown.appendChild(dropdownOption);
+});
